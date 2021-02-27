59521b83a54ef2875f3e749ca5e6f1999caca493
Siddhant Giri <sidgiri2000@gmail.com>
2021-02-27 23:49:05 +0530

    updated

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    DATABASE="mongodb+srv://siddhantgiri:sid55555@cluster0.r2scy.mongodb.net/merch?retryWrites=true&w=majority",
-SECRET="stacklancers"
-}
--- config/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    DATABASE=process.env.DATABASE,
-SECRET=process.env.SECRET
-}
